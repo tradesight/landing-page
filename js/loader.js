aaae6f52b8fe0daa1e8f8e896f52054611c83ba7
Tu Tran Nguyen Anh <anhtu1910@gmail.com>
2023-09-28 00:32:03 +0700

    feat(landing): favicon + improve CSS

--- js/loader.js
+++ js/loader.js
@@ -5,5 +5,8 @@ $(window).on('load', function () {
             // fadeOut complete. Remove the loading div
             $("#loadingDiv").remove(); //makes page more lightweight 
         });
+        $('html, body').css({
+            'overflow': 'auto',
+        });
     }, 2000); //wait for page load PLUS two seconds.
 });
